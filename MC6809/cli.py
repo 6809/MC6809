7251b005fd9a00d1f70119d828a28a7c6a43f1ee
JensDiemer <git@jensdiemer.de>
2015-05-26 13:24:05 +0200

    add cli for benchmark

---
+++ MC6809/cli.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+    MC6809 - CLI
+    ~~~~~~~~~~~~
+
+    :created: 2015 by Jens Diemer - www.jensdiemer.de
+    :copyleft: 2015 by the MC6809 team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+
+import sys
+
+try:
+    import click
+except ImportError as err:
+    print("Import error: %s" % err)
+    print()
+    print("Please install 'click' !")
+    print("more info: http://click.pocoo.org")
+    sys.exit(-1)
+
+from MC6809.core.bechmark import run_benchmark
+
+
+DEFAULT_LOOPS = 5
+@click.command(help="Run a 6809 Emulation benchmark")
+@click.option("--loops", default=DEFAULT_LOOPS,
+    help="How many benchmark loops should be run? (default: %i)" % DEFAULT_LOOPS)
+def benchmark(loops):
+    run_benchmark(loops)
+
+
+if __name__ == "__main__":
+    benchmark()
