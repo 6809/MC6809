5017b92bdb4d734ee0f9e6396375c034a74db1ac
JensDiemer <git@jensdiemer.de>
2015-05-26 13:42:53 +0200

    unittest run code

--- MC6809/cli.py
+++ MC6809/cli.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, division, print_function
 
 
 import sys
+import unittest
 
 try:
     import click
@@ -23,16 +24,29 @@ except ImportError as err:
     print("more info: http://click.pocoo.org")
     sys.exit(-1)
 
+from MC6809.tests import run_tests
 from MC6809.core.bechmark import run_benchmark
 
 
+@click.group()
+def cli():
+    pass
+
+
 DEFAULT_LOOPS = 5
-@click.command(help="Run a 6809 Emulation benchmark")
+@cli.command(help="Run a 6809 Emulation benchmark")
 @click.option("--loops", default=DEFAULT_LOOPS,
     help="How many benchmark loops should be run? (default: %i)" % DEFAULT_LOOPS)
 def benchmark(loops):
     run_benchmark(loops)
 
 
+@cli.command(help="Run unittests")
+@click.option('--verbosity', default=2, help='Number for verbosity settings')
+@click.option('--failfast', default=False, help='Number for verbosity settings', is_flag=True)
+def tests(verbosity, failfast):
+    run_tests(verbosity, failfast)
+
+
 if __name__ == "__main__":
-    benchmark()
+    cli()
---
+++ MC6809/tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def get_tests():
+    loader = unittest.TestLoader()
+    tests = loader.discover('.')
+    return tests
+
+
+def run_tests(verbosity=2, failfast=False):
+    tests = get_tests()
+
+    # test_runner = TextTestRunner2(
+    test_runner = unittest.TextTestRunner(
+        verbosity=verbosity,
+        failfast=failfast,
+    )
+    test_runner.run(tests)
