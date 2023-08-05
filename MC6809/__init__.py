99cb9a9dc49327281a95dcb5726280ab89e20af0
JensDiemer <git@jensdiemer.de>
2023-08-05 17:37:49 +0200

    Split CLI by apply manageprojects update

--- MC6809/__init__.py
+++ MC6809/__init__.py
@@ -1,7 +1,6 @@
-"""MC6809 - MC6809 CPU emulator written in Python"""
+"""
+    MC6809 CPU emulator written in Python
+"""
 
-from importlib.metadata import version
-
-
-__version__ = version('MC6809')
+__version__ = '0.7.0'
 __author__ = 'Jens Diemer <git@jensdiemer.de>'
